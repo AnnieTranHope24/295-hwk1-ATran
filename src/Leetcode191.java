9545f14ae73984aa88a4423abfc0b83649098475
AnnieNgocTran <ngoc.tran@hope.edu>
2023-01-14 10:47:05 -0500

    Added the implementation of the leetcode191 problem

--- src/Leetcode191.java
+++ src/Leetcode191.java
@@ -2,8 +2,25 @@
 public class Leetcode191 {
 
 	public static void main(String[] args) {
-
+		System.out.println(hammingWeight(-3));
+		System.out.println("Expected to be 31");
 
 	}
+	/*
+	 * A function that takes an unsigned integer
+	 * and returns the number of '1' bits it has (also known as the Hamming weight).
+	 */
+    // you need to treat n as an unsigned value
+    public static int hammingWeight(int n) {
+        String biString = Integer.toBinaryString(n);
+        int count = 0;
+        for(char c: biString.toCharArray()){
+            if(c == '1'){
+                count++;
+            }
+        }
+
+        return count;        
+    }
 
 }
